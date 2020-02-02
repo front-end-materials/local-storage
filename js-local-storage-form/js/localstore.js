cb9d8074beebc4d445d7c8fd02907706bb107324
Dave Everitt <deveritt@innotts.co.uk>
2020-02-02 20:50:33 +0000

    refactor main js function and create checkEmpty

--- js-local-storage-form/js/localstore.js
+++ js-local-storage-form/js/localstore.js
@@ -3,37 +3,43 @@
 (function(){
 
   const myName = document.getElementById("my-name"); 
-  const getName = document.getElementById("getName");
-  const userName = document.getElementById("userName"); 
+  // const getName = document.getElementById("getName");
+  // const userName = document.getElementById("userName"); 
   let nameStored = localStorage.getItem('name');
   
+  /*
+    If there's a name in localStorage use it,
+    otherwise show a placeholder:
+  */
   if(nameStored) {
     console.log(`Name on page load: ${nameStored}`);
-    // If there's a name in localStorage, use it:
     myName.innerText = `again ${nameStored}`;
     console.log(`Name stored is: ${nameStored}`);
-  }
-  else {
-    // Use a placeholder if no name in localStorage:
+  } else {
     myName.innerText = "stranger";
     console.log(`No name stored`);
   }
 
-  function greetUser() {
-    event.preventDefault();
+  function checkEmpty() {
     if(userName.value === "") {
       alert("Please enter a name");
       userName.focus();
     }
-    // Gets the name the user entered:
-    nameStored = userName.value;
+  }
+
+  function greetUser() {
+    // don't reload the page
+    event.preventDefault();
     
-    // Shows the name in "my-name":
+    checkEmpty();
+    
+    // Gets the name the user entered
+    // and displays it in "my-name":
+    nameStored = userName.value;
     myName.innerText = nameStored;
     
     // Puts the name into localStorage:
     localStorage.setItem('name', nameStored);
-    
     console.log(`New name stored: ${nameStored}`);
   }
 
